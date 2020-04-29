fa239189e3d8c0a4dafb1b73401a612dbb15c3d9
Luis Cesar <lutzcc1@gmail.com>
2020-04-29 15:42:36 -0500

    setup authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
-  has_many :meals, :bookings, :reviews
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :meals, dependent: :destroy
+  has_many :bookings, dependent: :destroy
+  has_many :reviews, dependent: :destroy
 end
