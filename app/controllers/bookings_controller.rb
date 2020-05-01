db61473de99d12ded5b23edb1a4f0685249d384b
Jose Luna <jmlunamugica@mac.com>
2020-05-01 17:20:43 -0500

    Created bookings controller, added index, show, edit, update and destroy methods.

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,40 @@
+class BookingsController < ApplicationController
+
+  def index
+    @bookings = Booking.all
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def edit
+    @booking = Booking.find(params[:id])
+  end
+
+  def update
+    @booking = Booking.find(params[:id])
+    @booking.update(bookings_params)
+
+    if @booking.update
+      redirect_to bookings_path(@booking)
+    else
+      render :new
+    end
+
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+
+    redirect_to bookings_path
+  end
+
+  private
+
+  def bookings_params
+    params.require(:booking).permit(:date, :eaters, :user_id, :meal_id)
+  end
+
+end
