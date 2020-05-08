ffbfb12cf08c4b3892e108f7356ad77ec6164532
David Osuna Azcona Dominguez <david.oad@gmail.com>
2020-05-07 20:15:25 -0600

    Added features and links to show

--- db/seeds.rb
+++ db/seeds.rb
@@ -41,7 +41,7 @@ meal_list.each do |name, address, picture|
       user_id: rand(1..10),
       open_hrs: ['8:00', '8:30', '9:00', '9:30', '10:00', '10:30', '11:00', '11:30', '12:00', '12:30'].sample,
       close_hrs: ['13:00', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:00', '17:30', '18:00', '18:30', '19:00'].sample,
-      open_days: ['Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun'].sample(rand(1..7))
+      open_days: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'].sample(rand(1..7))
       )
   file = URI.open(picture.to_s)
   meal.photos.attach(io: file, filename: 'meal.png', content_type: 'image/png')
